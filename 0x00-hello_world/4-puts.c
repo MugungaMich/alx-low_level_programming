5fbb2d3e4d03811c29942ac46a29339b5b9cb261
MugungaMich <mugungamich@gmail.com>
2023-02-16 10:42:41 -0800

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - this is the main function
+ *
+ * Return: always zero
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
